445b0ae4aed54bd82681636e8f88ddd27a6b6819
Gregory Marshall <gmarshall@dawan.fr>
2024-06-28 11:21:03 +0200

    functionnal component et transitions

--- src/vue/FunctionnalComp.vue
+++ src/vue/FunctionnalComp.vue
@@ -1,7 +1,27 @@
 <template>
 
+<FunctionnalButton @click="clickezMoi">Clickez Moi</FunctionnalButton>
+
 </template>
 
-<script>
+<script setup>
+
+import { h, defineComponent } from 'vue'
+
+const FunctionnalButton = defineComponent({
+    functionnal: true, 
+    props: {
+        onClick: Function
+    }, 
+    setup(props, { slots }) {
+        return () => h('button', { onClick: props.onClick }, slots);
+    }
+});
+
+const clickezMoi = () => {
+    alert('On m`a cliqué');
+}
+
+//export {FunctionnalButton} <- si déclaré dans un fichier javascript
 
 </script>
--- src/vue/Transition.vue
+++ src/vue/Transition.vue
@@ -1,7 +1,34 @@
 <template>
 
+<button @click="show = !show">Afficher/Cacher Paragraphe</button>
+
+<transition name="effacer">
+    <p v-if="show">Je suis un paragraphe qui va se cacher</p>
+</transition>
+
+
 </template>
 
 <script>
 
+export default {
+    data() {
+        return {
+            show: true
+        }
+    }
+}
+
 </script>
+
+<style>
+
+.effacer-enter-active, .effacer-leave-active {
+    transition: opacity 1s;
+}
+
+.effacer-enter-form, .effacer-leave-to {
+    opacity: 0;
+}
+
+</style>
